32a3cd300428df586ca730d3b6a555be01af2243
MufyDhaBeee <mufeedabeegum93@gmail.com>
2026-06-15 16:43:54 +0530

    completing sample movie app ui for api integrating completed

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -4,25 +4,6 @@ import 'package:logger/logger.dart';
 import 'package:movies_api/model/details_model.dart';
 import 'package:movies_api/model/most_popular.dart';
 
-Future<MoviesDetails> fetchCarouselDetails() async {
-  final response = await http.get(
-    Uri.parse('https://imdb236.p.rapidapi.com/api/imdb/tt0816692'),
-    headers: {
-      'Accept': 'application/json',
-      'x-rapidapi-host': 'imdb236.p.rapidapi.com',
-      'x-rapidapi-key': 'd175a0e844msh3bb7746748bf9b2p1fb3cfjsn2e24dc3232f4',
-    },
-  );
-  print(response.body);
-
-  if (response.statusCode == 200) {
-    return MoviesDetails.fromJson(
-      jsonDecode(response.body) as Map<String, dynamic>,
-    );
-  } else {
-    throw Exception('Failed to load weather');
-  }
-}
 
 Future<List<MostPopularMovieList>> fetchMostPopular() async {
   Logger logger = Logger();
